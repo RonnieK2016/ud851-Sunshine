6ccf0cc92bd83f3f6ac5b71fdde73e5d15eb9aeb
SergeA <ronnieo@gmail.com>
2018-01-11 22:48:32 -0500

    Lesson 2 Completed

--- S01.02-Exercise-AddWeatherList/app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ S01.02-Exercise-AddWeatherList/app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -17,20 +17,45 @@ package com.example.android.sunshine;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    // TODO (1) Create a field to store the weather display TextView
+    // COMPLETED Create a field to store the weather display TextView
+    TextView myWeatherData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forecast);
 
-        // TODO (2) Use findViewById to get a reference to the weather display TextView
+        // COMPLETED Use findViewById to get a reference to the weather display TextView
+        myWeatherData = (TextView) findViewById(R.id.tv_weather_data);
 
-        // TODO (3) Create an array of Strings that contain fake weather data
+        // COMPLETED Create an array of Strings that contain fake weather data
+        String[] weatherData = new String[]{
+                "Atlanta, Georgia            22",
+                "Austin, Texas               27",
+                "Baltimore, Maryland         18",
+                "Birmingham, Alabama         23",
+                "Boston, Massachusetts       15",
+                "Buffalo, New York           14",
+                "Charlotte, North Carolina   22",
+                "Chicago, Illinois           15",
+                "Cincinnati, Ohio            18",
+                "Cleveland, Ohio             15",
+                "Columbus, Ohio              17",
+                "Dallas, Texas               25",
+                "Denver, Colorado            18",
+                "Detroit, Michigan           15",
+                "Hartford, Connecticut       16",
+                "Houston, Texas              27",
+                "Indianapolis, Indiana       17"
+        };
 
-        // TODO (4) Append each String from the fake weather data array to the TextView
+        // COMPLETED Append each String from the fake weather data array to the TextView
+        for (String city : weatherData) {
+            myWeatherData.append(city + "\n\n");
+        }
     }
 }
